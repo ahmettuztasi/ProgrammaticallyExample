655fb603d0336cfee4a10f4e8d4cb3612a907ed1
ahmettuztasi <tuztasiahmet@gmail.com>
2020-04-17 15:18:36 +0300

    project created

---
+++ ProgrammaticallyExample/ProgrammaticallyExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticallyExample
+//
+//  Created by admin on 17.04.2020.
+//  Copyright © 2020 ahmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
